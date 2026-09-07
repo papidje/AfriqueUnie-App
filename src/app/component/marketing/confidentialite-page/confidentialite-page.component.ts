f28d73cacddd0c9526b45c8e785b229cdc81560f
DOUMBOUYA Séré Moussa <dseremoussa@gmail.com>
2026-09-08 01:57:10 +0200

    ajout pages informations et acceuil

---
+++ src/app/component/marketing/confidentialite-page/confidentialite-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confidentialite-page',
+  templateUrl: './confidentialite-page.component.html',
+  styleUrls: ['./confidentialite-page.component.scss']
+})
+export class ConfidentialitePageComponent {}
---
+++ src/app/component/marketing/mentions-legales-page/mentions-legales-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mentions-legales-page',
+  templateUrl: './mentions-legales-page.component.html',
+  styleUrls: ['./mentions-legales-page.component.scss']
+})
+export class MentionsLegalesPageComponent {}
